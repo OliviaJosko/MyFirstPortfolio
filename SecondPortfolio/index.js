e2cb458db2b843d1c9b08f92b81d4bebaecaaf2c
Olivia Josko <olivia_marta.josko@smail.th-koeln.de>
2024-09-21 21:17:22 +0200

    new slider

--- SecondPortfolio/index.js
+++ SecondPortfolio/index.js
@@ -29,49 +29,14 @@ navLink.forEach( n => n.addEventListener('click', linkAction))
 
 
 /*================= Uni Projects Slider ====================*/
-let items = document.querySelectorAll('.slider .items'); // Korrigiere den Selektor auf 'items'
-let active = 3;
+document.addEventListener('DOMContentLoaded', function() {
+    const swiper = new Swiper('.swiper', {
+        loop: true,
 
-function loadShow() {
-    items[active].style.transform = `none`;
-    items[active].style.zIndex = 1;
-    items[active].style.filter = 'none';
-    items[active].style.opacity = 1;
-
-    // show after
-    let stt = 0;
-    for (var i = active + 1; i < items.length; i++) {
-        stt++;
-        items[i].style.transform = `translateX(${120 * stt}px) scale(${1 - 0.2 * stt}) perspective(16px) rotateY(-1deg)`;
-        items[i].style.zIndex = -stt;
-        items[i].style.filter = 'blur(5px)';
-        items[i].style.opacity = stt > 2 ? 0 : 0.6;
-    }
-    stt = 0;
-    for (var i = active - 1; i >= 0; i--) {
-        stt++;
-        items[i].style.transform = `translateX(${-120 * stt}px) scale(${1 - 0.2 * stt}) perspective(16px) rotateY(1deg)`;
-        items[i].style.zIndex = -stt;
-        items[i].style.filter = 'blur(5px)';
-        items[i].style.opacity = stt > 2 ? 0 : 0.6;
-    }
-}
-
-loadShow();
-
-let next = document.getElementById('next');
-let prev = document.getElementById('prev');
-
-next.onclick = function () {
-    // Update active with circular logic
-    active = active + 1 < items.length ? active + 1 : 0;
-    loadShow();
-}
-
-prev.onclick = function () {
-    // Update active with circular logic
-    active = active - 1 >= 0 ? active - 1 : items.length - 1;
-    loadShow();
-}
-
-
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+});
